e28fbd8f55d85676edda9a02527896f7369f03b2
Ionut007Alexandru <116071172+Ionut007Alexandru@users.noreply.github.com>
2024-04-09 16:26:00 +0300

    Add files via upload

---
+++ persons.py
@@ -0,0 +1,7 @@
+persons = [
+    {"name": "Hiriscau Ionut", "button_code": "ember1169"},
+    {"name": "Natanael Iosif Balogh", "button_code": "ember231"},
+    {"name": "Codrut Borcutean", "button_code": "ember349"}
+
+]
+
